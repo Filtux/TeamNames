95e6b222298846dee864919f9d7bfaee4037f2dd
Joe Matthews <joematthews966@gmail.com>
2024-05-13 23:21:07 +0930

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('team_input.html') # Default
+
+@app.route('/live')
+def live_page():
+    return render_template('live_page.html')
+
+@socketio.on('new_team')
+def handle_new_team(data):
+    team_name = data['teamName']
+    team_number = data['teamNumber']
+    emit('new_team_added', {'teamName': team_name, 'teamNumber': team_number}, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
